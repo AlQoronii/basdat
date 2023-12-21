6421da6f5fa49650a65e8f5cdcc17347cb48d375
Muhammad Fathurrozak Al Qoroni <116018304+AlQoronii@users.noreply.github.com>
2023-12-21 14:24:18 +0700

    test

--- Pages/admin/tanggapan/index.php
+++ Pages/admin/tanggapan/index.php
@@ -1,23 +1,42 @@
 <?php
+include 'functions/class/tanggapan.php';
+
 $database = new Database();
 $connection = $database->getConnection();
-// Query untuk mengambil data tanggapan
+// Query to retrieve tanggapan data
+$tanggapan = new Tanggapan($connection);
+$edit = $tanggapan->editTanggapan();
+
+$delete = $tanggapan->delete();
+
+
 $queryTanggapan = "SELECT t.*, p.laporan, m.nama AS nama_masyarakat, petugas.nama_petugas
                    FROM tanggapan t
                    INNER JOIN pengaduan p ON t.id_pengaduan = p.id_pengaduan
                    INNER JOIN masyarakat m ON p.nik = m.nik
                    INNER JOIN petugas ON t.id_petugas = petugas.id_petugas
-                   ORDER BY t.tgl_tanggapan DESC"; // Sesuaikan dengan kolom yang diperlukan
+                   ORDER BY t.tgl_tanggapan DESC"; // Adjust with the required columns
 
-// Melakukan query ke database
+// Perform the database query
 $resultTanggapan = odbc_exec($connection, $queryTanggapan);
 
-// Mengecek apakah query berhasil dieksekusi
+// Check if the query was successful
 if (!$resultTanggapan) {
     die("Query failed: " . odbc_errormsg());
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-TygpuPtXnWshq8Uwru4ZrBfDewHxqzWu2Kluz9o1ur5An1LQSLtmo8gO3yZK7uf" crossorigin="anonymous">
+    <title>List of Responses</title>
+</head>
+
+<body>
 
 <!-- ================ List of Responses/Tanggapan ================ -->
 <div class="main">
@@ -25,8 +44,6 @@ if (!$resultTanggapan) {
         <div class="toggle">
             <ion-icon name="menu-outline"></ion-icon>
         </div>
-
-
 
         <div class="user">
             <img src="assets/imgs/customer01.jpg" alt="">
@@ -36,17 +53,16 @@ if (!$resultTanggapan) {
         <div class="recentOrders">
             <div class="cardHeader">
                 <h2>List of Responses</h2>
-
             </div>
 
-            <table>
+            <table class="table">
                 <thead>
                     <tr>
-                        <td>Nama Masyarakat</td>
-                        <td>Laporan</td>
-                        <td>Tanggapan</td>
-                        <td>Nama Petugas</td>
-                        <td>Aksi</td>
+                        <th>Nama Masyarakat</th>
+                        <th>Laporan</th>
+                        <th>Tanggapan</th>
+                        <th>Nama Petugas</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
 
@@ -60,7 +76,37 @@ if (!$resultTanggapan) {
                             <td><?php echo $rowTanggapan['tanggapan']; ?></td>
                             <td><?php echo $rowTanggapan['nama_petugas']; ?></td>
                             <td>
+                                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#myModal<?= $rowTanggapan['id_tanggapan']; ?>"><i class="fa fa-pencil-square"></i></button>
+                                <a href="index.php?page=tanggapan&id_pengaduan=<?= $rowTanggapan['id_tanggapan'] ?>" onclick="javascript:return confirm('Hapus Data Buku?');" class="btn btn-danger btn-xs m-1"><i class="fa fa-trash"></i></a>
 
+                                <!-- Modal -->
+                                <div id="myModal<?= $rowTanggapan['id_tanggapan']; ?>" class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Edit Tanggapan</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+
+                                            <form method="post" action="index.php?page=tanggapan">
+                                                <div class="modal-body">
+                                                    <p><strong>Nama:</strong> <?= $rowTanggapan['nama_masyarakat'] ?></p>
+                                                    <p><strong>Laporan:</strong> <?= $rowTanggapan['laporan'] ?></p>
+                                                    <div class="mb-3">
+                                                        <label for="tanggapan<?= $rowTanggapan['id_tanggapan'] ?>" class="form-label">Tanggapan:</label>
+                                                        <textarea class="form-control" id="tanggapan<?= $rowTanggapan['id_tanggapan'] ?>" name="tanggapan"></textarea>
+                                                    </div>
+                                                </div>
+                                                <input type="hidden" name="id_pengaduan" id="id_pengaduan" value="<?= $rowTanggapan['id_pengaduan'] ?>">
+                                                
+                                                <div class="modal-footer">
+                                                    <button type="submit" name="submit" class="btn btn-primary">Submit Response</button>
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     <?php
@@ -71,3 +117,8 @@ if (!$resultTanggapan) {
         </div>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-rX1cpqtrqVDIZ0+TnFEW+DvBiHP8z9f8+O2brKnrjL5W93IA0E6Z9vZyMQ5iQZXP" crossorigin="anonymous"></script>
+</body>
+
+</html>
